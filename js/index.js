e3bb367ff9d90219e7cf68aa50ead6bd0fe1ae5a
hangjang <frontweb99@gmail.com>
2022-12-19 18:03:14 +0900

    221219
    footer
    footer pop-up
    slider

--- js/index.js
+++ js/index.js
@@ -19,3 +19,35 @@ $(function(){
         $('.pop-rightbox').animate({marginLeft:-2700},800);
     })
 });
+
+$(function(){
+    $('.thirdfooter>li:nth-of-type(3)>a').click(function(){
+        $('#popupcheck').fadeToggle();
+    });
+});
+
+$(function(){
+    $('#popupcheck>a').click(function(){
+        $('#popupcheck').fadeOut();
+    });
+});
+
+
+function pre(){
+    $('.slide li:last').prependTo('.slide');
+    $('.slide').css("margin-left","-100%");
+    $('.slide').stop().animate({marginLeft:0},2000);
+}
+function next(){
+    $('.slide').stop().animate({marginLeft:-1900},2000, function(){
+        $('.slide li:first').appendTo('.slide');
+        $('.slide').css({marginLeft:0});
+    });
+}
+setInterval(next,3000);
+$('.pre').click(function(){
+    prev();
+});
+$('.next').click(function(){
+    next();
+});
