a97046c1a69c161ecf04042f063f442e97d19afc
hangjang <frontweb99@gmail.com>
2022-12-20 18:32:49 +0900

    221220
    슬라이더 애니메이션 버튼 누르면 넘어가기
    하얀 화살표 위에 마우스 올리면 빨간색으로 바꾸기
    마우스 올리면 영어에서 한글로 바꾸기

--- js/index.js
+++ js/index.js
@@ -33,21 +33,108 @@ $(function(){
 });
 
 
+
+
+
 function pre(){
     $('.slide li:last').prependTo('.slide');
     $('.slide').css("margin-left","-100%");
-    $('.slide').stop().animate({marginLeft:0},2000);
+    $('.slide').stop().animate({marginLeft:0},850);
 }
 function next(){
-    $('.slide').stop().animate({marginLeft:-1900},2000, function(){
+    $('.slide').stop().animate({marginLeft:"-100%"},850, function(){
         $('.slide li:first').appendTo('.slide');
         $('.slide').css({marginLeft:0});
     });
 }
-setInterval(next,3000);
-$('.pre').click(function(){
-    prev();
+setInterval(next,4000);
+
+$(function(){
+    $('.pre').click(function(){
+        pre();
+    });
+    $('.pre1').click(function(){
+        pre();
+    });
+
+    $('.next').click(function(){
+        next();
+    });
+    $('.next1').click(function(){
+        next();
+    })
 });
-$('.next').click(function(){
-    next();
+
+$(function(){
+    $('.pre>img').hover(function(){
+        $(this).hide();
+        $('.pre1>img').show();
+    }, function(){
+        $(this).show();
+        $('.pre1>img').hide();
+    });
+    $('.next>img').hover(function(){
+        $(this).hide();
+        $('.next1>img').show();
+    }, function(){
+        $(this).show();
+        $('.next1>img').hide();
+    })
+
 });
+
+
+ //선택자가 같은 경우 한 번에 써줘도 됨 .text().css() / css로 효과를 주는게 많은 경우 .css()로 묶어서 써줘도 됨 .css({"css":"효과","css":"효과"}) / 아니면 그냥 .text().css().css() 이렇게 써도 됨
+
+$(function () {
+    // BRAND 메뉴들
+    $('.brandhover>ul>li:nth-of-type(1)>a').hover(function () {
+        $(this).text("아웃백 이야기").css({ fontFamily: "'Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"});
+    }, function () {
+        $(this).text("OUTBACK STORY").css("fontFamily", "Barlow', sans-serif").css("fontSize", "1.1rem");
+    });
+
+    $('.brandhover>ul>li:nth-of-type(2)>a').hover(function () {
+        $(this).text("아웃백 책임경영").css({ fontFamily: "'Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"})
+    }, function () {
+        $(this).text("OUTBACK CSR").css("fontFamily", "Barlow', sans-serif").css("fontSize", "1.1rem");
+    });
+
+    // MENU 메뉴들
+    $('.menuhover>ul>li:nth-of-type(1)>a').hover(function () {
+        $(this).text("스테이크&세트").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"});
+    }, function () {
+        $(this).text("STEAK & FAVORITE").css({"fontFamily":"Barlow', sans-serif","fontSize":"1.1rem","fontWeight":"bold"})
+    })
+
+    $('.menuhover>ul>li:nth-of-type(2)>a').hover(function(){
+        $(this).text("파스타 & 샐러드").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"});  
+    }, function(){
+        $(this).text("PASTA & SALADS").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"})
+    });
+
+    $('.menuhover>ul>li:nth-of-type(3)>a').hover(function(){
+        $(this).text("와인").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"});  
+    }, function(){
+        $(this).text("WINES").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"})
+    });
+
+    $('.menuhover>ul>li:nth-of-type(4)>a').hover(function(){
+        $(this).text("디저트 & 기타").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"});  
+    }, function(){
+        $(this).text("DESSERTS & OTHERS").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"})
+    });
+
+    $('.menuhover>ul>li:nth-of-type(5)>a').hover(function(){
+        $(this).text("점심 세트").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"});  
+    }, function(){
+        $(this).text("LUNCH SET").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"})
+    });
+
+    $('.menuhover>ul>li:nth-of-type(6)>a').hover(function(){
+        $(this).text("배달").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"});  
+    }, function(){
+        $(this).text("DELIIVERY").css({"fontFamily": "Barlow', sans-serif", "fontSize": "1.1rem", "fontWeight": "bold"})
+    });
+});
+
